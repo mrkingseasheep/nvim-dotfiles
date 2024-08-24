7d538ec3ec0ec55d12c8e7f297e80577bf156149
Xinyang Wang <136667147+mrkingseasheep@users.noreply.github.com>
2024-08-23 23:17:05 -0400

    luasnip works

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -4,4 +4,26 @@ return {
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
 	build = "make install_jsregexp",
+	lazy = false,
+	config = function()
+		local ls = require("luasnip")
+
+		vim.keymap.set({ "i" }, "<C-K>", function()
+			ls.expand()
+		end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-L>", function()
+			ls.jump(1)
+		end, { silent = true })
+		vim.keymap.set({ "i", "s" }, "<C-J>", function()
+			ls.jump(-1)
+		end, { silent = true })
+
+		vim.keymap.set({ "i", "s" }, "<C-E>", function()
+			if ls.choice_active() then
+				ls.change_choice(1)
+			end
+		end, { silent = true })
+		require("luasnip.loaders.from_snipmate").lazy_load({ paths = "~/.config/nvim/snippets" })
+		-- require("luasnip.loaders.from_snipmate").load()
+	end,
 }
